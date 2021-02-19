30cdad04c7504e5ec092d5d00f3b920ead43385d
User <User@LAPTOP-48AVBV65>
2021-02-19 21:55:07 +0800

    added sitemap

--- BAIT2113-WAD/script.js
+++ BAIT2113-WAD/script.js
@@ -1,5 +1,6 @@
 ﻿function showNavMenu() {
     var x = document.getElementById("navi-menu");
+
     if (x.style.display == "block") {
         setTimeout(function () { x.style.display = "none"; }, 400);
     } else {
